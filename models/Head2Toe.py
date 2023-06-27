7df6ee0c0f09e566499a3cf68f3d5be851f24063
HumzaW245 <humzaw28@gmail.com>
2023-06-27 11:57:35 -0700

    head2toe py file to be updated using colab version

--- models/Head2Toe.py
+++ models/Head2Toe.py
@@ -28,13 +28,13 @@ class Net(torch.nn.Module):
         if self.finetune_backbones == False:
             helper.freezeBackbone(self.model)
         
-        # New output head
-        in_features = self.model.fc.in_features
-        targetTaskOutFeatures = helper.numUniqueClasses(self.datasetName)
-        classifier = nn.Linear(in_features, targetTaskOutFeatures, bias=True)
+        #New output head
+        in_features = self.model.fc.in_features #The fc layer of resenet50 is Linear(in_features=2048, out_features=1000, bias=True) so storing the 2048 and replacing this to map from 2048 to numClasses for target task ====can see the fc layer like this: backbone = models.resnet50(pretrained=True) => print(backbone.fc)
+        targetTaskOutFeatures = helper.numUniqueClasses(self.datasetName) # num of classes in target task        
+        classifier = nn.Linear(in_features, targetTaskOutFeatures, bias=True)  # Create a new classifier
         classifier.weight.requires_grad = True
         classifier.bias.requires_grad = True
-        self.model.fc = classifier
+        self.model.fc = classifier  # Replace the classifier layer
 
         # Get the layers of the model
         
@@ -42,15 +42,42 @@ class Net(torch.nn.Module):
         #self.layers = list(self.model.children()) #Use .modules() instead of .children() if want to also access layers within layers. (nested layers) since children() looks at layers but there may be a sequential layer among these which contains other layers. to access the nested sequential layer, modules() is more appropriate
 
     def forward(self, x):
-        for idx, module in enumerate(self.model.children()):
-            if idx ==  9: #The flatten is not a layer so have to do it ourselves here. Search "x = torch.flatten(x, 1)" in https://pytorch.org/vision/0.8/_modules/torchvision/models/resnet.html
-                #print(f"Reached Layer {idx} so flattening tensor")
-                # Flatten the output tensor
-                x = torch.flatten(x, 1)
-            
-            x = module(x)
-            #print(f"Layer #: {idx} and module name is {module}")
-            #print(f"Layer output shape: {x.shape}")
+        
+        '''
+        ##############
+
+        DONT ACTUALLY NEED TO GET EVERY LAYER....
+        
+        JUST NEED TO GET INDICES OF NEURONS FROM EACH LAYER THAT WILL BE USED.
+
+        ONCE YOU MAKE A CONCATENATED LAYER OF ALL THOSE NEURONS e.g. (l1_n23, l1_n53, l2_n3, ...),
+        in the forward pass can continue just doing model(x) and have model.fc = Identity
+
+        so then once done preTrained model(x), it stops, gets the concatenated neurons layer
+
+        and do that layer as the 'x' for a new classifier Linear(neuronsInNewLayer, 10)
+
+        THEN  in backward, it will go from the 10 outputs to the concatenated layer and each neuron from there goes back
+        to whatever was used to compute it and updates accordingly in optimizer step.
+        ##############
+
+
+        '''
+        # See https://pytorch.org/vision/0.8/_modules/torchvision/models/resnet.html
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+
+        x = self.model.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.model.fc(x)
+
         return x
 
 
